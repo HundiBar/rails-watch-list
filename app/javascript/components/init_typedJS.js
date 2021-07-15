8669cc51c9e18ef21512de18a8ddb743a4d2337c
Dean Abbott <deanabbott293@gmail.com>
2021-07-15 15:50:58 +0900

    centred tYpedJS

--- app/javascript/components/init_typedJS.js
+++ app/javascript/components/init_typedJS.js
@@ -2,11 +2,11 @@ import Typed from 'typed.js'
 
 const initTypedJS = () => {
   new Typed('#lower-header', {
-    strings: ["All your favorite watches in one place...", "Chill with friends...", "Chill with 😉..."],
-    typeSpeed: 100,
+    strings: ["All your favorite watches in one place...", "Chill with friends...", "Chill with that special someone 😉..."],
+    typeSpeed: 80,
     loop: true,
     smartBackspace:true,
-    backSpeed: 80,
+    backSpeed: 60,
   });
 }
 
